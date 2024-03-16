ef97a73a0ea770c6b0680e0c9ef1aa849e8af097
WassCodeur <wachioubouraima56@gmail.com>
2024-03-16 12:39:32 +0000

    Refactor code to improve performance and readability

---
+++ frontend/assets/js/index.js
@@ -0,0 +1,4 @@
+const current_date = new Date();
+const current_year = current_date.getFullYear();
+const copy = document.getElementById('copy');
+copy.innerHTML = `&copy; ${current_year} International House of Issue Templates`
