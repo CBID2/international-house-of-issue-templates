9ab90945a2508d8e443f3065adda46d8c998dd9d
WassCodeur <wachioubouraima56@gmail.com>
2024-03-24 16:14:30 +0000

    Update file paths and add fetch function for template retrieval

--- frontend/assets/js/submit.js
+++ frontend/assets/js/submit.js
@@ -1,17 +1,24 @@
-const fs = require('fs');
-const fileArYal = "../src/templates/yaml/template-ar.yaml"
-const fileArMd = "../src/templates/markdown/template-ar.md"
-const fileEnYal = "../src/templates/yaml/template-en.yaml"
-const fileEnMd = "../src/templates/markdown/template-en.md"
-const fileFrYal = "../src/templates/yaml/template-fr.yaml"
-const fileFrMd = "../src/templates/markdown/template-fr.md"
-const fileEsYal = "../src/templates/yaml/template-es.yaml"
-const fileEsMd = "../src/templates/markdown/template-es.md"
-
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+const fileArYal = "../frontend/assets/src/templates/yaml/template-ar.yaml"
+const fileArMd = "../frontend/assets/src/templates/markdown/template-ar.md"
+const fileEnYal = "../frontend/assets/src/templates/yaml/template-en.yaml"
+const fileEnMd = "../frontend/assets/src/templates/markdown/template-en.md"
+const fileFrYal = "../frontend/assets/src/templates/yaml/template-fr.yaml"
+const fileFrMd = "../frontend/assets/src/templates/markdown/template-fr.md"
+const fileEsYal = "../frontend/assets/src/templates/yaml/template-es.yaml"
+const fileEsMd = "../frontend/assets/src/templates/markdown/template-es.md"
+const generator = document.getElementById("generator");
+function getTemplate(file) {
+    fetch()
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error : ' + response.status);
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('error :', error);
+        });
+}
