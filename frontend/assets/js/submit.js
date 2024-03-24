d130a504c3e45d664f5f65b6d0420b0a77330602
WassCodeur <wachioubouraima56@gmail.com>
2024-03-24 18:56:15 +0000

    Refactor code to improve template selection and value assignment

--- frontend/assets/js/submit.js
+++ frontend/assets/js/submit.js
@@ -1,19 +1,13 @@
-const fileArYal = "../frontend/assets/src/templates/yaml/template-ar.yaml"
-const fileArMd = "../frontend/assets/src/templates/markdown/template-ar.md"
-const fileEnYal = "../frontend/assets/src/templates/yaml/template-eng.yaml"
-const fileEnMd = "../frontend/assets/src/templates/markdown/template-eng.md"
-const fileFrYal = "../frontend/assets/src/templates/yaml/template-fr.yaml"
-const fileFrMd = "../frontend/assets/src/templates/markdown/template-fr.md"
-const fileEsYal = "../frontend/assets/src/templates/yaml/template-es.yaml"
-const fileEsMd = "../frontend/assets/src/templates/markdown/template-es.md"
 const generator = document.getElementById("generate");
 const lang = document.getElementById("language-select");
 const type = document.getElementById("file-type-select");
 
 
 function getTemplate(file) {
+    const template = document.getElementById("issueDescription");
     fetch(file)
         .then(response => {
+
             if (!response.ok) {
                 throw new Error('Error : ' + response.status);
             }
@@ -21,6 +15,7 @@ function getTemplate(file) {
         })
         .then(data => {
             console.log(data);
+            template.value = data;
         })
         .catch(error => {
             console.error('error :', error);
@@ -28,42 +23,42 @@ function getTemplate(file) {
 }
 
 function checkLangAnFile() {
-     if (lang.value === "ar" && type.value === "yaml") {
-         return "../frontend/assets/src/templates/yaml/template-ar.yaml";
-     } else if (lang.value === "ar" && type.value === "md") {
-         return "../frontend/assets/src/templates/markdown/template-ar.md";
-     } else if (lang.value === "en" && type.value === "yaml") {
-         return "../frontend/assets/src/templates/yaml/template-eng.yaml";
-     } else if (lang.value === "en" && type.value === "md") {
-         return "../frontend/assets/src/templates/markdown/template-eng.md";
-     } else if (lang.value === "fr" && type.value === "yaml") {
-         return "../frontend/assets/src/templates/yaml/template-fr.yaml";
-     } else if (lang.value === "fr" && type.value === "md") {
-         return "../frontend/assets/src/templates/markdown/template-fr.yaml";
-     } else if (lang.value === "sp" && type.value === "yaml") {
-         return "../frontend/assets/src/templates/yaml/template-es.yaml";
-     } else if (lang.value === "sp" && type.value === "md") {
-         return "../frontend/assets/src/templates/markdown/template-es.md";
-     }else{
+    if (lang.value === "ar" && type.value === "yaml") {
+        return "../frontend/assets/src/templates/yaml/template-ar.yaml";
+    } else if (lang.value === "ar" && type.value === "md") {
+        return "../frontend/assets/src/templates/markdown/template-ar.md";
+    } else if (lang.value === "en" && type.value === "yaml") {
+        return "../frontend/assets/src/templates/yaml/template-eng.yaml";
+    } else if (lang.value === "en" && type.value === "md") {
+        return "../frontend/assets/src/templates/markdown/template-eng.md";
+    } else if (lang.value === "fr" && type.value === "yaml") {
+        return "../frontend/assets/src/templates/yaml/template-fr.yaml";
+    } else if (lang.value === "fr" && type.value === "md") {
+        return "../frontend/assets/src/templates/markdown/template-fr.yaml";
+    } else if (lang.value === "es" && type.value === "yaml") {
+        return "../frontend/assets/src/templates/yaml/template-es.yaml";
+    } else if (lang.value === "es" && type.value === "md") {
+        return "../frontend/assets/src/templates/markdown/template-es.md";
+    } else {
         return null
-     }
+    }
 }
 
 generator.addEventListener("click", function (e) {
     e.preventDefault()
+
     const file = checkLangAnFile();
-    if(file !== null){
-        console.log(lang.value);
-        console.log(type.value);
-        console.log(file);
+    if (file !== null) {
+
         getTemplate(file);
+        lang.value = ""
+        type.value = ""
+        //window.location.href = "/frontend/pages/template.html";
+    } else {
+        alert("Please select a language and a file type")
     }
-   
+
 
 });
 
-function submitForm() {
-    const form = document.getElementById("form");
-    form.submit();
-}
 
---
+++ frontend/assets/src/templates/markdown/template-es.md
@@ -0,0 +1,27 @@
+# Título del problema
+
+## Descripción
+
+Por favor, proporciona una descripción detallada del problema.
+
+## Pasos para reproducir
+
+1. Paso 1
+2. Paso 2
+3. Paso 3
+
+## Resultado esperado
+
+Describe lo que esperabas que sucediera.
+
+## Resultado actual
+
+Describe lo que sucedió en realidad.
+
+## Posibles soluciones
+
+Si tienes alguna idea de cómo solucionar este problema, por favor compártela aquí.
+
+## Información adicional
+
+Incluye cualquier otra información que pueda ser útil para resolver este problema.
