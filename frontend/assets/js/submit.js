e78da19a24937008a882648f43481b55ce7e34cf
WassCodeur <wachioubouraima56@gmail.com>
2024-03-24 15:06:26 +0000

    Update submit.js and index.html

--- frontend/assets/js/submit.js
+++ frontend/assets/js/submit.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+const reader = new FileReader();
+
+const generateBtn = document.getElementById('generate');
 const fileArYal = "../src/templates/yaml/template-ar.yaml"
 const fileArMd = "../src/templates/markdown/template-ar.md"
 const fileEnYal = "../src/templates/yaml/template-en.yaml"
@@ -8,10 +10,17 @@ const fileFrMd = "../src/templates/markdown/template-fr.md"
 const fileEsYal = "../src/templates/yaml/template-es.yaml"
 const fileEsMd = "../src/templates/markdown/template-es.md"
 
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
+// fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+reader.readAsText(fileArYal);
+
+console.log(generateBtn)
+generateBtn.addEventListener('click', () => {
+    console.log('Button clicked');
+})
