a30e501f11c043a760dd4398207482119da2a5d5
WassCodeur <wachioubouraima56@gmail.com>
2024-03-24 14:39:28 +0000

    Delete unused files and settings

--- frontend/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const current_date = new Date();
-const current_year = current_date.getFullYear();
-const copy = document.getElementById('copy');
-copy.innerHTML = `&copy; ${current_year} International House of Issue Templates`
-
-copy.innerHTML = `&copy; ${current_year} International House of Issue Templates`
-
